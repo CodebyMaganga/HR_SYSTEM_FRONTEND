da3d0b56e0a2b36be552db2bea5c7c9b2f377a2d
David <dave.obat@student.moringashool.com>
2024-02-20 17:42:19 +0300

    bottom and mid cards added

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { IoIosPeople } from "react-icons/io";
+import { IoIosPeople, IoIosBriefcase, IoIosPaper, IoIosChatbubbles, IoIosCheckmarkCircleOutline } from "react-icons/io";
 import {BASE_URL} from "./utils"
 
 
@@ -14,14 +14,50 @@ function Card() {
   
   }, []); 
   return (
-    <div className="flex flex-wrap justify-center relative top-[-16px] z-0">
-      <div className="max-w-sm rounded overflow-hidden shadow-lg m-4 text-center ">
-        <div className="px-6 py-4 grid place-items-center ">
-          <IoIosPeople className="size-12  " />
-
-          <div className="font-bold text-xl mb-2 mt-4">Total Employees</div>
+    <div className="flex justify-start -mt-20 z-50 overflow-x-auto border-solid">
+      {/* Card 1: Total Employees */}
+      <div className="flex-none w-64 rounded-[15px] overflow-hidden shadow-lg m-4 ">
+        <div className="px-4 py-4">
+          <IoIosPeople className=" text-3xl" />
+          <div className="font-bold text-sm mb-2 mt-4">Total Employees</div>
+          <p className="text-gray-700 text-lg">200/200</p>
         </div>
-        <p className="text-gray-700 text-lg">200/200</p>
+      </div>
+
+      {/* Card 2: Employees on Leave */}
+      <div className="flex-none w-64 max-w-sm rounded-[15px] overflow-hidden shadow-lg m-4  ">
+        <div className="px-4 py-4">
+          <IoIosBriefcase className=" text-3xl" />
+          <div className="font-bold text-sm mb-2 mt-4">On Leave</div>
+          <p className="text-gray-700 text-lg">15/200</p>
+        </div>
+      </div>
+
+      {/* Card 3: Projects */}
+      <div className="flex-none  w-64 max-w-sm rounded-[15px] overflow-hidden shadow-lg m-4  ">
+        <div className="px-4 py-4">
+          <IoIosPaper className=" text-3xl" />
+          <div className="font-bold text-sm mb-2 mt-4">Projects</div>
+          <p className="text-gray-700 text-lg">8 Active</p>
+        </div>
+      </div>
+
+      {/* Card 4: Interviews */}
+      <div className="flex-none w-64 max-w-sm rounded-[15px] overflow-hidden shadow-lg m-4  ">
+        <div className="px-4 py-4">
+          <IoIosChatbubbles className=" text-3xl" />
+          <div className="font-bold text-sm mb-2 mt-4">Interviews</div>
+          <p className="text-gray-700 text-lg">5 Today</p>
+        </div>
+      </div>
+
+      {/* Card 5: Job Applicants */}
+      <div className="flex-none w-64 max-w-sm rounded-[15px] overflow-hidden shadow-lg m-4  ">
+        <div className="px-4 py-4">
+          <IoIosCheckmarkCircleOutline className=" text-3xl" />
+          <div className="font-bold text-sm mb-2 mt-4">Applicants</div>
+          <p className="text-gray-700 text-lg">20 New</p>
+        </div>
       </div>
     </div>
   );
